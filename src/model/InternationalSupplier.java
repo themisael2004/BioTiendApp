f5b36ddfaca98bd5aa86363a1b9886fe08301bd6
Lizzie Peña <bp24005@ues.edu.sv>
2025-05-23 23:00:57 -0600

    Creacion de la clase InternationalSupplier

---
+++ src/model/InternationalSupplier.java
@@ -0,0 +1,21 @@
+package model;
+
+public class InternationalSupplier extends Supplier {
+    private final String supplierType = "International";
+
+    public InternationalSupplier (int idSupplier, String nameSupplier, String supplierDescription, String supplierDirection, String supplierCity, String supplierCountry, String supplierContact) {
+        super(idSupplier, nameSupplier, supplierDescription, supplierDirection, supplierCity, supplierCountry, supplierContact);
+    }
+
+@Override
+public void createSupplier() {
+    System.out.println("Proveedor locarl creado");
+}
+
+@Override
+public void readSupplier() {
+    System.out.println("Datos del Proveedor Local: ");
+    System.out.println(this.toString());
+    System.out.println("Tipo: " + supplierType);
+}
+}
