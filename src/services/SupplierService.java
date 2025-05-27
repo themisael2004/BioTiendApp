ef0e8663fc4b7075c5afd77c14b52d9b91b50382
themisael2004 <softwaremisael@gmail.com>
2025-05-27 17:56:07 -0600

    feat: Modificacion de metodo

--- src/services/SupplierService.java
+++ src/services/SupplierService.java
@@ -10,13 +10,16 @@ import java.util.List;
 public class SupplierService {
     private List<Supplier> suppliers = new ArrayList<>();
 
-    public void createLocalSupplier(int id, String name, String description, String direction, String city, String country, String contact, String regionalCode) {
+    public void createLocalSupplier(int id, String name, String description, String direction, String city,
+            String country, String contact, String regionalCode) {
         LocalSupplier local = new LocalSupplier(id, name, description, direction, city, country, contact, regionalCode);
         suppliers.add(local);
     }
-    
-    public void createInternacionalSupplier(int id, String name, String direction, String city, String country, String contact, String isoCode){
-        InternationalSupplier intl = new InternationalSupplier(id, name, direction, city, country, contact, isoCode, country);
+
+    public void createInternacionalSupplier(int id, String name, String direction, String city, String country,
+            String contact, String isoCode) {
+        InternationalSupplier intl = new InternationalSupplier(id, name, direction, city, country, contact, contact,
+                isoCode);
         suppliers.add(intl);
     }
 
