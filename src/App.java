ba3ba4b2a605898252e9fb8aed5a8124c85d8e37
themisael2004 <softwaremisael@gmail.com>
2025-06-03 20:23:13 -0600

    feat: modificacion

--- src/App.java
+++ src/App.java
@@ -4,10 +4,7 @@ import ui.SupplierUi;
 public class App {
     public static void main(String[] args) throws Exception {
           SupplierService supplierService = new SupplierService();
-
         SupplierUi suplierUi = new SupplierUi(supplierService);
-
-        // 3. Iniciar la interfaz de usuario
         suplierUi.runSupplierMenu();
     }
 }
